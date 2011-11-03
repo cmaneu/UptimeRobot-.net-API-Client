b4c56c8695824814fcb77d14960fb4b3eb4ed921
Christopher Maneu <christopher@maneu.net>
2011-11-03 22:13:58 +0100

    Adding GetMonitor() + refactoring

--- src/UptimeRobotClient/UptimeRobotContext.cs
+++ src/UptimeRobotClient/UptimeRobotContext.cs
@@ -31,9 +31,38 @@ namespace maneu.tools.UptimeRobotClient
 
         public List<Monitor> GetMonitors()
         {
+            return RequestMonitor();
+        }
+
+        public Monitor GetMonitor(string monitorId)
+        {
+            return RequestMonitor(monitorId).FirstOrDefault();
+        }
+
+        public void AddMonitor()
+        {
+            
+        }
+
+        public void UpdateMonitor()
+        {
+            
+        }
+
+        public void DeleteMonitor()
+        {}
+
+
+        private List<Monitor> RequestMonitor(string monitorId = null)
+        {
             StringBuilder sb = new StringBuilder(_baseUri);
             sb.Append("/getMonitors?");
             sb.AppendFormat("apiKey={0}", _apiKey);
+            if (monitorId != null)
+            {
+                sb.AppendFormat("&monitors={0}", monitorId);    
+            }
+
             sb.AppendFormat("&logs={0}", 1);
             sb.AppendFormat("&format={0}", _responseFormat);
 
@@ -44,36 +73,19 @@ namespace maneu.tools.UptimeRobotClient
 
             var monitors = from m in xDoc.Descendants("monitor")
                            select new Monitor()
-                                      {
-                                          Id = Convert.ToInt32(m.Attribute("id").Value),
-                                          FriendlyName = m.Attribute("friendlyname").Value,
-                                          Type = (MonitorType)Enum.Parse(typeof(MonitorType), m.Attribute("type").Value),
-                                          Subtype = m.Attribute("subtype").Value !="" ? (MonitorSubtype)Enum.Parse(typeof(MonitorSubtype), m.Attribute("subtype").Value) : MonitorSubtype.Unknow,
-                                          CurrentStatus = (Status)Enum.Parse(typeof(Status), m.Attribute("status").Value),
-                                          UptimeRatio = Double.Parse(m.Attribute("alltimeuptimeratio").Value,NumberStyles.AllowDecimalPoint,CultureInfo.InvariantCulture),
-                                          KeywordValue = m.Attribute("keywordvalue").Value,
-                                          Port = m.Attribute("port").Value=="" ? 0 : Convert.ToInt32(m.Attribute("port").Value)
-                                      };
-           
+                           {
+                               Id = Convert.ToInt32(m.Attribute("id").Value),
+                               FriendlyName = m.Attribute("friendlyname").Value,
+                               Type = (MonitorType)Enum.Parse(typeof(MonitorType), m.Attribute("type").Value),
+                               Subtype = m.Attribute("subtype").Value != "" ? (MonitorSubtype)Enum.Parse(typeof(MonitorSubtype), m.Attribute("subtype").Value) : MonitorSubtype.Unknow,
+                               CurrentStatus = (Status)Enum.Parse(typeof(Status), m.Attribute("status").Value),
+                               UptimeRatio = Double.Parse(m.Attribute("alltimeuptimeratio").Value, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture),
+                               KeywordValue = m.Attribute("keywordvalue").Value,
+                               Port = m.Attribute("port").Value == "" ? 0 : Convert.ToInt32(m.Attribute("port").Value)
+                           };
+
             return monitors.ToList();
         }
-
-        public void GetMonitor()
-        {
-            
-        }
-
-        public void AddMonitor()
-        {}
-
-        public void SaveMonitor()
-        {
-            
-        }
-
-        public void DeleteMonitor()
-        {}
-
 
     }
 
